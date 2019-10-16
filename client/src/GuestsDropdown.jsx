a372a952fad7b2285534af07991c3b56f197a923
Craig Shih <cshih1141@gmail.com>
2019-10-15 22:16:09 -0700

    guest dropdown styling complete

--- client/src/GuestsDropdown.jsx
+++ client/src/GuestsDropdown.jsx
@@ -2,21 +2,24 @@ import React from'react';
 
 const GuestsDropdown = ({guestVisibility}) => (
   <div className="guestsDropdown" id="guestsDropdown" style={{visibility: guestVisibility}}>
-    <div id="adultGuestDropdown">
+    <div className="typeOfGuest" id="adultGuestDropdown">
       <span className="guestType" id="adultGuest">Adults</span>
       <button className="minusButton" id="adultMinus">-</button>
+      <span className="guestCounter" id="adultCounter">0</span>
       <button className="addButton" id="adultAdd">+</button>
     </div>
-    <div id="childGuestDropdown">
+    <div className="typeOfGuest" id="childGuestDropdown">
       <span className="guestType" id="childGuest">Children</span>
       <span className="guestDetails" id="childDetails">Ages 2-12</span>
       <button className="minusButton" id="childMinus">-</button>
+      <span className="guestCounter" id="childCounter">0</span>
       <button className="addButton" id="childAdd">+</button>
     </div>
-    <div id="infantGuestDropdown">
+    <div className="typeOfGuest" id="infantGuestDropdown">
       <span className="guestType" id="infantGuest">Infants</span>
       <span className="guestDetails" id="infantDetails">Under 2</span>
       <button className="minusButton" id="infantMinus">-</button>
+      <span className="guestCounter" id="infantCounter">0</span>
       <button className="addButton" id="infantAdd">+</button>
     </div>
     <div className="guestsDropdownFooter" id="guestsDropdownFooter">
